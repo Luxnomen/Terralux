63bd553f4fd1b42c5b2c42d92ef7fbbaadc0daa7
Luxnomen <praisepxnk@outlook.com>
2026-01-19 10:40:52 -0800

    Update engine.c

--- src/engine.c
+++ src/engine.c
@@ -1,25 +1,27 @@
 #include "engine.h"
 #include "raymath.h"
 
+void Engine_Init(Entity *e) {
+    e->transform.pos = (Vector3){ 0, 10, 0 };
+    e->velocity = (Vector3){ 0, 0, 0 };
+    e->isGrounded = false;
+}
+
 void Engine_ApplyGravity(Entity *e, float gravity) {
     float dt = GetFrameTime();
-    if (!e->isGrounded) {
-        e->velocity.y -= gravity * dt; // Constant pull down
+    if (e->transform.pos.y > 0) {
+        e->velocity.y -= gravity * dt;
+        e->isGrounded = false;
+    } else {
+        e->transform.pos.y = 0;
+        e->velocity.y = 0;
+        e->isGrounded = true;
     }
 }
 
 void Engine_ResolveMovement(Entity *e, Vector3 inputDir, float speed) {
     float dt = GetFrameTime();
-    float friction = 10.0f;
-
-    // Apply Acceleration based on input
-    e->velocity.x += inputDir.x * speed * dt;
-    e->velocity.z += inputDir.z * speed * dt;
-
-    // Apply Friction (This gives that "heavy" Frostbite feel)
-    e->velocity.x -= e->velocity.x * friction * dt;
-    e->velocity.z -= e->velocity.z * friction * dt;
-
-    // Update Position
-    e->transform.position = Vector3Add(e->transform.position, e->velocity);
+    e->velocity.x = inputDir.x * speed * dt;
+    e->velocity.z = inputDir.z * speed * dt;
+    e->transform.pos = Vector3Add(e->transform.pos, e->velocity);
 }
