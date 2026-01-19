98b78f98329b6783f99440a3c099976048fbb39b
Luxnomen <praisepxnk@outlook.com>
2026-01-19 11:10:12 -0800

    Update engine.h

--- include/engine.h
+++ include/engine.h
@@ -2,27 +2,12 @@
 #define ENGINE_H
 #include "raylib.h"
 
-#define MAX_ENTITIES 100
-
 typedef struct {
-    Vector3 pos; 
+    Vector3 position;
     float yaw;
     float pitch;
-} EngineTransform;
+    int health;
+    int itemsCollected;
+} Player;
 
-typedef struct {
-    EngineTransform transform;
-    Vector3 velocity;
-    Color color;
-    bool active; // Is this object in the game right now?
-} Entity;
-
-typedef struct {
-    Entity entities[MAX_ENTITIES];
-    int count;
-} World;
-
-void Engine_InitWorld(World *w);
-void Engine_SpawnEntity(World *w, Vector3 position, Color color);
-void Engine_UpdateWorld(World *w, Entity *player);
 #endif
